5fd5bfdf07503666eb7974a99a13ecbef41893ed
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2024-06-05 00:40:10 +0100

    data man

---
+++ 0x00-ES6_basic/1-main.js
@@ -0,0 +1,4 @@
+import returnHowManyArguments from "./4-rest-parameter"
+
+console.log(returnHowManyArguments("1","qq", "qjj", "kkqkq","qjjj","qjjjw","qkqkkqkq"))
+console.log(returnHowManyArguments("a", "b"))
---
+++ 0x00-ES6_basic/main5.js
@@ -0,0 +1,5 @@
+import concatArrays from "./5-spread-operator";
+
+const ar1 = [1, 2, 3]
+const ar2 = ["a", "b"]
+console.log(concatArrays(ar1, ar2, "stringy"))
